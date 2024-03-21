25102dcffbfe047c5851fcf733a3218fb0d4765c
Jimna254 <jimmynarokian254@gmail.com>
2024-03-21 13:31:46 +0300

    feat : img animation

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,18 +1,20 @@
- main
 import Home from "./pages/landing/Home";
 import AdminSidebar from "./components/AdminSidebar/AdminSidebar";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Event from "./pages/event/Event";
+import Room from "./pages/rooms/Room";
+import Meeting from "./pages/Meetings/Meeting";
 
 function App() {
   return (
     <div className="main-app">
-
       {/* <Home /> */}
-      {/* <Room/> */}
+      {/* <Room /> */}
       {/* <Restaurant /> */}
-  <Event/>
-
+      {/* <Event /> */}
       {/* <AdminSidebar /> */}
-      <AboutUs />
+      {/* <AboutUs /> */}
+      <Meeting />
     </div>
   );
 }
