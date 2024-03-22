563a8dfc4ffbba5234aea0d992df4c88ce89e27f
frank-Mwangi <kungofrank@gmail.com>
2024-03-22 10:13:19 +0300

    Resolved merge conflicts

--- FrontEnd/src/pages/restaurant/Restaurant.jsx
+++ FrontEnd/src/pages/restaurant/Restaurant.jsx
@@ -1,46 +1,41 @@
-import React from 'react'
-import './Restaurant.scss'
-import RestaurantContent from '../../components/restaurant/RestaurantContent'
-import image from '../../assets/Restaurant/image 8.png'
-import Navbar from '../../components/shared/Navbar'
-
+import React from "react";
+import "./Restaurant.scss";
+import RestaurantContent from "../../components/restaurant/RestaurantContent";
+import image from "../../assets/Restaurant/image 8.png";
+import Navbar from "../../components/shared/Navbar";
 
 const Restaurant = () => {
   return (
     <div className="mainRestaurant">
-        <Navbar/>   
-        <div className="image">
-            <img
-                src={image}
+      {/* <Navbar/>    */}
+      <div className="image">
+        <img src={image} alt="ola" />
+      </div>
 
-                alt="ola" 
-            />
+      <div className="descr">
+        <div className="words">
+          <p>
+            Savour the spirit of Africa at Bantu Africa's restaurant, where
+            local, organic ingredients meet the art of open flame grilling,
+            creating a dining experience that's a journey through Kenya's
+            vibrant culture and traditions, all against the serene backdrop of
+            Mount Kenya.
+          </p>
         </div>
-
-        <div className="descr">
-            <div className="words">
-                <p>Savour the spirit of Africa at Bantu Africa's restaurant, where local, organic ingredients meet the art of open flame grilling, creating a dining experience that's a journey through Kenya's vibrant culture and traditions, all against the serene backdrop of Mount Kenya.
-                </p>
-            </div>
-            <div className="buttons">
-                <button className="btn1">Offers</button>
-                <button className="btn2">Our Menu</button>
-                <button className="btn1">Enquiries</button>
-                
-            </div>
-
+        <div className="buttons">
+          <button className="btn1">Offers</button>
+          <button className="btn2">Our Menu</button>
+          <button className="btn1">Enquiries</button>
         </div>
+      </div>
 
-        <div className="openhours">
-            <p className="name">BANTU AFRICA RESTAURANT</p>
-            <p className="time">Opening Hours: 6:00am to 1:00am</p>
-
-        </div>
-    <RestaurantContent />
+      <div className="openhours">
+        <p className="name">BANTU AFRICA RESTAURANT</p>
+        <p className="time">Opening Hours: 6:00am to 1:00am</p>
+      </div>
+      <RestaurantContent />
     </div>
-    
-    
-  )
-}
+  );
+};
 
-export default Restaurant
+export default Restaurant;
