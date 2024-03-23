d17242c201c8ad90fe0cdc287d329231d4f07e06
unknown <emmqnuelkipsqng@gmail.com>
2024-03-23 13:24:02 +0300

    feat: activity store

--- FrontEnd/src/app/Store.js
+++ FrontEnd/src/app/Store.js
@@ -4,15 +4,17 @@ import { setupListeners } from "@reduxjs/toolkit/query/react";
 
 import { roomApi } from "../features/rooms/roomApi";
 import { servicesApi } from "../features/services/servicesApi";
+import { activityApi } from "../features/activities/activityApi";
 
 export const store = configureStore({
   reducer: {
     [roomApi.reducerPath]: roomApi.reducer,
     [servicesApi.reducerPath]: servicesApi.reducer,
+    [activityApi.reducerPath]: activityApi.reducer
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(roomApi.middleware, servicesApi.middleware),
+    getDefaultMiddleware().concat(roomApi.middleware, servicesApi.middleware, activityApi.middleware),
 });
 
 setupListeners(store.dispatch);
