b39e90b9c089ea187b7800d952cd28cd8d5ae471
unknown <emmqnuelkipsqng@gmail.com>
2024-03-23 13:14:10 +0300

    feat: activity api

--- FrontEnd/src/features/activities/activityApi.js
+++ FrontEnd/src/features/activities/activityApi.js
@@ -1,10 +1,49 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-export const activityApi =createApi({
+
+export const activityApi = createApi({
     reducerPath: "activityApi",
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/'}),
-    tagTypes: ['actities'],
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/' }),
+    tagTypes: ['activities'],
     endpoints: (builder) => ({
-        getActivities: builder.query
+
+        getActivities: builder.query({
+            query: () => 'activities',
+            providesTags: ['activities']
+        }),
+
+        addActivity: builder.mutation({
+            query: (activity) => ({
+                url: 'activity',
+                method: 'POST',
+                body: activity
+            }),
+            invalidatesTags: ['activities']
+        }),
+
+        updateActivity: builder.mutation({
+            query: (activity) => ({
+                url: `activity/${activity.ActivityId}`,
+                method: 'PUT',
+                body: activity
+            }),
+            invalidatesTags: ['activities']
+        }),
+
+        deleteActivity: builder.mutation({
+            query: (ActivityId) => ({
+                url: `activity/${ActivityId}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['activities']
+        })
+        
     })
-    
 })
+
+export const { 
+    useGetActivitiesQuery,
+    useAddActivityMutation,
+    useUpdateActivityMutation,
+    useDeleteActivityMutation
+} = activityApi;
+
