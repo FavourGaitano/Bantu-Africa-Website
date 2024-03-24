fea1dc5225ef4f5b02450bc5d90293e50054ab0b
unknown <emmqnuelkipsqng@gmail.com>
2024-03-24 17:09:32 +0300

    feat: create other service

---
+++ BackEnd/src/controllers/otherServicesController.js
@@ -0,0 +1,43 @@
+
+import { v4 } from 'uuid';
+import { otherServiceValidator } from '../validators/otherSevicesValidator.js';
+import { createOtherService } from '../services/otherServices.js';
+
+export const createOtherServiceController = async (req, res) => {
+  try {
+    const { OtherServiceName, Description, ImageUrl } = req.body;
+    console.log(req.body);
+    const { error } = otherServiceValidator({ OtherServiceName, Description, ImageUrl });
+
+    if (error) {
+      return res.json({ message: error.message });
+    }
+
+    const OtherServiceId = v4();
+    const CreatedAt = new Date().toISOString();
+
+    const newOtherService = {
+      OtherServiceId,
+      OtherServiceName,
+      Description,
+      ImageUrl,
+      CreatedAt,
+    };
+
+    const response = await createOtherService(newOtherService);
+    console.log("response:", response);
+    if (response.message) {
+      return res.json({
+        message: response.message,
+      });
+    } else {
+      return res.status(201).json({
+        message: "Other service created successfully",
+        service: newOtherService,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
---
+++ BackEnd/src/services/otherServices.js
@@ -0,0 +1,38 @@
+import { poolRequest, sql } from "../utils/dbConnect.js";
+
+export const createOtherService = async (newOtherService) => {
+    try {
+        const checkOtherServiceQuery = `
+            SELECT COUNT(*) AS count
+            FROM tbl_other_service
+            WHERE OtherServiceId = @OtherServiceId
+        `;
+
+        const checkOtherServiceResult = await poolRequest()
+            .input("OtherServiceId", sql.VarChar, newOtherService.OtherServiceId)
+            .query(checkOtherServiceQuery);
+
+        if(checkOtherServiceResult.recordset[0].count > 0){
+            throw new Error('The other service already exists!')
+        }
+
+        //adding other service to db
+        const addOtherServiceQuery = `
+            INSERT INTO tbl_other_service (OtherServiceId, OtherServiceName, Description, ImageUrl, CreatedAt)
+            VALUES (@OtherServiceId, @OtherServiceName, @Description, @ImageUrl, @CreatedAt)
+        `;
+
+        const result = await poolRequest()
+            .input("OtherServiceId", sql.VarChar, newOtherService.OtherServiceId)
+            .input("OtherServiceName", sql.VarChar, newOtherService.OtherServiceName)
+            .input("Description", sql.VarChar, newOtherService.Description)
+            .input("ImageUrl", sql.VarChar, newOtherService.ImageUrl)
+            .input("CreatedAt", sql.VarChar, newOtherService.CreatedAt)
+            .query(addOtherServiceQuery)
+        console.log("result",result);
+        return result
+    } catch (error) {
+        return error.message;
+        
+    }
+};
