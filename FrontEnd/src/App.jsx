278170ef1263ed8aba9ddf332c3353331111d59a
unknown <emmqnuelkipsqng@gmail.com>
2024-03-25 06:55:10 +0300

    feat: display the offer modal

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -11,12 +11,12 @@ import Offers from "./components/Offers/Offers";
 function App() {
   return (
     <div className="main-app">
-      {/* <Routes>
+      <Routes>
         <Route path="/*" element={<Main/>} />
         <Route path='/admin/*' element={<AdminRoute/>} />
         <Route path="/menu" element={<Menu/>} />
-      </Routes> */}
-      <Offers />
+      </Routes>
+      {/* <Offers /> */}
    
  
          </div>
--- FrontEnd/src/pages/landing/Home.jsx
+++ FrontEnd/src/pages/landing/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "../../components/shared/Carousel";
 import "./home.scss";
 import Carousel2 from "../../components/shared/Carousel2";
@@ -8,17 +8,36 @@ import SpecialOffers from "../../components/home/SpecialOffers";
 import Footer from "../../components/shared/Footer";
 import Map from "../../components/home/Map";
 import OtherServices from "../../components/OtherServices/OtherServices";
+import Offers from "../../components/Offers/Offers";
 
 const Home = () => {
+  const [showOffersModal, setShowOffersModal] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowOffersModal(true);
+    }, 3000);
+
+    return () => clearTimeout(timer); // Cleanup function to clear the timeout if component unmounts
+  }, []);
+
+  const handleCloseModal = () => {
+    setShowOffersModal(false);
+  };
+
   return (
     <div className="home-page">
-
+      <div className={`modal-background ${showOffersModal ? "show" : ""}`}>
+        <button className="close-icon" onClick={handleCloseModal}>
+          X
+        </button>
+        <div className="modal-content">{showOffersModal && <Offers />}</div>
+      </div>
       <Carousel />
       <MarketingCards />
       <SpecialOffers />
       {/* <OtherServices /> */}
       <Map />
-     
     </div>
   );
 };
