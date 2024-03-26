30265fc943a4ba23888ff6c9ac31af335e48b485
Jimna254 <jimmynarokian254@gmail.com>
2024-03-26 17:16:16 +0300

    feat: Worked on Contact us and Events responsiveness

--- BackEnd/src/routes/roomCategoryRoute.js
+++ BackEnd/src/routes/roomCategoryRoute.js
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
-import { Router } from 'express';
-import { createRoomCategoryController, deleteRoomCategoryController, findRoomCategory, getCategoriesController, getPriceController, getRoomCategroryByIdController, softDeleteRoomCategoryController, updateRoomCategoryController } from '../controllers/roomCategoryController.js';
-=======
 import { Router } from "express";
 import {
   createRoomCategoryController,
   deleteRoomCategoryController,
   findRoomCategory,
-  getCategoriesController,
+  getPriceController,
   getRoomCategroryByIdController,
   softDeleteRoomCategoryController,
   updateRoomCategoryController,
 } from "../controllers/roomCategoryController.js";
->>>>>>> c335ed8f217674c353f55063c4c8d53047078678
 const roomCategoryRouter = Router();
 
 roomCategoryRouter.post("/category", findRoomCategory);
-roomCategoryRouter.get("/rooms/category", getCategoriesController);
 roomCategoryRouter.post("/rooms/category", createRoomCategoryController);
-roomCategoryRouter.post('/rooms/category/price', getPriceController);
+roomCategoryRouter.post("/rooms/category/price", getPriceController);
 
 roomCategoryRouter.put(
   "/rooms/category/update/:RoomCategoryId",
@@ -37,11 +31,4 @@ roomCategoryRouter.delete(
   deleteRoomCategoryController
 );
 
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> c335ed8f217674c353f55063c4c8d53047078678
 export default roomCategoryRouter;
