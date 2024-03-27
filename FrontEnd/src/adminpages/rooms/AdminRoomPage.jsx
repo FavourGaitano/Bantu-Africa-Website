17350d05b9833884951fd213e98e79e3b1278e0a
MeshackKorir <meshackkorir100@gmail.com>
2024-03-27 03:42:41 +0300

    fix

--- FrontEnd/src/adminpages/rooms/AdminRoomPage.jsx
+++ FrontEnd/src/adminpages/rooms/AdminRoomPage.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import AdminTable from '../../components/AdminTable/AdminTable';
 import { useGetRoomsQuery } from '../../features/rooms/roomApi';
+import Button from '../../components/shared/Button';
+import { FaRegSquarePlus } from "react-icons/fa6";
+import './AdminRoomPage.scss';
+import { useNavigate } from 'react-router-dom';
 
 const AdminRoomPage = () => {
-
+const navigate=useNavigate()
   const{data:rooms}=useGetRoomsQuery();
   console.log();
-
+const handleAddForm=()=>{
+  navigate('/admin/addroom')
+}
   return (
     <div>
+      <div className="admin-room-top">
       <h2>This is Admin Room Page</h2>
+      <div className="admin-room-btn-group">
+      <Button btnIcon={<FaRegSquarePlus/>} className="btn-add-room" msg="Add Category"/>
+      <Button btnIcon={<FaRegSquarePlus/>} onClick={handleAddForm} className="btn-add-room" msg="Add Room"/>
+      </div>
+      </div>
      {rooms && rooms.map((room)=>(
       <div key={room.RoomId}>
          <AdminTable
